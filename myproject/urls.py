eddb375f7e2df7839e3d32e3a2a42f0db3be7506
heet9201 <heetdedakiya80@gmail.com>
2022-04-26 09:59:57 +0530

    deployed

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,13 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from django.urls import include, re_path
+from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.views.static import serve
+from django.conf import settings
 
 
 urlpatterns = [
     path('', include("myapp.urls")),
     path('admin/', admin.site.urls),
+    
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':
+    settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':
+    settings.STATIC_ROOT}),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
